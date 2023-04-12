630b9f8ffc847a912a6ece159341f5d29185ea0d
Michele Lombardi <michele.lombardi2@unibo.it>
2023-04-12 18:16:12 +0200

    Some fixes

--- sol/bacino_idrico.py
+++ sol/bacino_idrico.py
@@ -2,9 +2,9 @@ import numpy as np
 from scipy import integrate
 
 def q(t, a=4, b=2, c=1):
-    return a + b * np.sin(2*np.pi*t/24) + b*np.sin(2*np.pi*t/13)
-
+    return a + b * np.sin(2*np.pi*t/24) + c * np.sin(2*np.pi*t/13)
 
 def immissione(f, t0, t1):
-    Q, error_ub = integrate.quad(f, t0, t1)
+    Q, eb = integrate.quad(f, t0, t1)
     return Q
+
